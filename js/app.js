52ad32a7b5677e4f6dcca5c288b542c26b02595b
root <sandeep.k@betsol.com>
2016-08-22 07:25:59 +0000

    Init

---
+++ js/app.js
@@ -0,0 +1,50 @@
+var app = angular.module('swiftCodeApp',['ngRoute','ngCookies']); // dependency injection
+var URL="http://betsol.org:9000/";
+
+//Routing
+app.config(function($routeProvider) {
+ $routeProvider // similar to switch
+  .when('/', {
+      redirectTo: '/login'
+  })
+  .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'loginCtrl'
+  })
+  .when('/signup', {
+      templateUrl: 'views/signup.html',
+      controller: 'signupCtrl'
+  })
+  .when('/dashboard', {
+      templateUrl: 'views/dashboard.html',
+      controller: 'dashboardCtrl'
+  })
+  .otherwise({
+      redirectTo: '/'
+  });
+});
+
+//Signup controller
+app.controller('signupCtrl', ['$scope', '$location', '$http', function($scope, $location, $http) {
+  this.signupData = {};
+  $scope.signup = function() {
+    var request = $http({
+      method: 'POST',
+      url: URL + "signup",
+      data: this.signupData
+    });
+    request.success(function(data){
+        var response = angular.fromJson(data);
+        console.log(response);
+    });
+    request.error(function(data){
+      console.log(data);
+    });
+  }
+}]);
+
+app.controller('loginCtrl', ['$scope', '$location', '$http', '$cookies', function($scope, $location, $http, $cookies) {
+}]);
+
+app.controller('dashboardCtrl', ['$scope', '$location', '$http', '$cookies', function($scope, $location, $http, $cookies) {
+}]);
